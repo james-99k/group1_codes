bb756fe2b3422108063089a8871a6aea95e9c4f5
james-99k <jamesgasty@gmail.com>
2021-04-05 18:27:05 +0800

    new java code added

---
+++ random_array/src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(boyName() + relationships() + girlName());
+    }
+    public static void boyName (){
+
+    }
+
+    public static void girlName(){
+
+    }
+
+    public static void relationships(){
+
+    }
+
+    public static int display(){
+        Random dice = new Random();
+        return dice.nextInt(5);
+    }
+}
